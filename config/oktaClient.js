70198197a21c337c75e6ac3a02123f042daf2701
Khaled <khaledesam384@gmail.com>
2025-07-28 08:58:16 +0300

    feat (prisma): prisma schema and migrations generated

--- config/oktaClient.js
+++
@@ -1,9 +0,0 @@
-// oktaClient.js
-const { Client } = require('@okta/okta-sdk-nodejs');
-
-const oktaClient = new Client({
-  orgUrl: `https://${process.env.OKTA_DOMAIN}`, // Replace with your Okta domain
-  token: process.env.OKTA_READ_API_TOKEN,       // Replace with your Okta API token
-});
-
-module.exports = oktaClient;
